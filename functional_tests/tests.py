8d76868d84b8967511f4f87195cfc53679e8e1fc
Jessica Gage <jemgage@gmail.com>
2020-08-12 14:38:02 -0600

    Made an app called functional_tests, renamed functional_tests.py to tests.py within the directory, and now using LiveServerTestCase instead of unittest TestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,9 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
-import unittest
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
 	def setUp(self):
 		self.browser = webdriver.Firefox()
@@ -20,7 +20,7 @@ class NewVisitorTest(unittest.TestCase):
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		#Edith has heard about a cool new to-do app. She goes to check out its homepage
 		
-		self.browser.get('http://localhost:8000')
+		self.browser.get(self.live_server_url)
 
 		#She notices the page title and header mention to-do lists
 
@@ -68,6 +68,3 @@ class NewVisitorTest(unittest.TestCase):
 		# She visits that URL and her to-do list is still there
 
 		#She goes back to sleep
-
-if __name__ == '__main__':
-	unittest.main(warnings='ignore')
