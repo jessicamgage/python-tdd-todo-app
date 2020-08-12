eaf579ea6e25216a0251eecd7dc44eea3bd0b791
Jessica Gage <jemgage@gmail.com>
2020-08-12 16:42:50 -0600

    Added a new URL separate from the home URL where one can view their list, and started progressing towards different visitors having different URLS

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -105,7 +105,7 @@ class NewVisitorTest(LiveServerTestCase):
 		#Francis visits the home page. There is no sign of Edith's list
 
 		self.browser.get(self.live_server_url)
-		page_text = self.browser_find_element_by_tag_name('body').text
+		page_text = self.browser.find_element_by_tag_name('body').text
 		self.assertNotIn('Buy peacock feathers', page_text)
 		self.assertNotIn('make a fly', page_text)
 
