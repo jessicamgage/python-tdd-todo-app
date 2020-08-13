eb4c8d40adfd5c9dcae0958b871c46ea38e825ec
Jessica Gage <jemgage@gmail.com>
2020-08-13 15:59:45 -0600

    Changed structure of files so that a List can now be called and supported

--- lists/tests.py
+++ lists/tests.py
@@ -2,10 +2,9 @@ from django.template.loader import render_to_string
 from django.urls import resolve
 from django.test import TestCase
 from django.http import HttpRequest
+from lists.models import Item, List
 
 from lists.views import home_page
-
-from lists.models import Item
 
 class HomePageTest(TestCase):
 
@@ -13,15 +12,19 @@ class HomePageTest(TestCase):
 		response = self.client.get('/')
 		self.assertTemplateUsed(response, 'home.html')
 
-class ItemModelTest(TestCase):
-
+class ListAndItemModelTest(TestCase):
 	def test_saving_and_retrieving_items(self):
+		list_ = List()
+		list_.save()
+
 		first_item = Item()
 		first_item.text = 'The first (ever) list item'
+		first_item.list = list_
 		first_item.save()
 
 		second_item = Item()
 		second_item.text = 'Item the second'
+		second_item.list = list_
 		second_item.save()
 
 		saved_items = Item.objects.all()
@@ -29,8 +32,11 @@ class ItemModelTest(TestCase):
 		
 		first_saved_item = saved_items[0]
 		second_saved_item = saved_items[1]
+
 		self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+		self.assertEqual(first_saved_item.list, list_)
 		self.assertEqual(second_saved_item.text, 'Item the second')
+		self.assertEqual(second_saved_item.list, list_)
 
 class ListViewTest(TestCase):
 
@@ -39,8 +45,9 @@ class ListViewTest(TestCase):
 		self.assertTemplateUsed(response, 'list.html')
 
 	def test_displays_all_items(self):
-		Item.objects.create(text='itemey 1')
-		Item.objects.create(text='itemey 2')
+		list_ = List.objects.create()
+		Item.objects.create(text='itemey 1', list=list_)
+		Item.objects.create(text='itemey 2', list=list_)
 		
 		response = self.client.get('/lists/the-only-list-in-the-world/')
 		
