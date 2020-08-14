6210815d65c9b743733b5fc22b91140333e3d97d
Jessica Gage <jemgage@gmail.com>
2020-08-14 17:04:32 -0600

    First steps of testing the layout using FTs

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -30,6 +30,27 @@ class NewVisitorTest(LiveServerTestCase):
 				time.sleep(0.5)
 
 
+	def test_layout_and_styling(self):
+		self.browser.get(self.live_server_url)
+		self.browser.set_window_size(1024, 768)
+
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertAlmostEqual(
+			inputbox.location['x'] + inputbox.size['width'] / 2,
+			512,
+			delta=10
+		)
+
+		inputbox.send_keys('testing')
+		inputbox.send_keys(Keys.ENTER)
+		self.wait_for_row_in_list_table('1: testing')
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertAlmostEqual(
+			inputbox.location['x'] + inputbox.size['width'] /2,
+			512,
+			delta=10
+		)
+
 	def test_can_start_a_list_for_one_user(self):
 		#Edith has heard about a cool new to-do app. She goes to check out its homepage
 		
