d667f33b37553610cf60086c4bce528dbafde61b
Jessica Gage <jemgage@gmail.com>
2020-08-19 13:53:20 -0600

    Hacked FT running to be able to test staging

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -5,10 +5,15 @@ import time
 
 from selenium.common.exceptions import WebDriverException
 
+import os
+
 class NewVisitorTest(StaticLiveServerTestCase):
 
 	def setUp(self):
 		self.browser = webdriver.Firefox()
+		staging_server = os.environ.get('STAGING_SERVER')
+		if staging_server:
+			self.live_server_url = 'http://' + staging_server	
 
 	def tearDown(self):
 		self.browser.quit()
