7e97de10d16cacf2dd5eb48c78a5082b63d82f7d
Jessica Gage <jemgage@gmail.com>
2020-08-27 15:44:59 -0600

    Finished ensuring that empty list items throw an error when they attempt to be entered into the list.

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -38,6 +38,3 @@ class ItemValidationTest(FunctionalTest):
 		self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
 		self.wait_for_row_in_list_table('1: Buy almond milk')
 		self.wait_for_row_in_list_table('2: Make tea')		
-
-
-		self.fail('write me!')
--- lists/views.py
+++ lists/views.py
@@ -1,26 +1,35 @@
 from django.shortcuts import render, redirect
 from django.core.exceptions import ValidationError
 from lists.models import Item, List
+from django.utils.html import escape
 
 def home_page(request):
 	return render(request, 'home.html')
 
 def view_list(request, list_id):
 	list_ = List.objects.get(id=list_id)
+	error = None
+
 	if request.method == 'POST':
-		Item.objects.create(text=request.POST['item_text'], list=list_)
-		return redirect(f'/lists/{list_.id}/')
-	return render(request, 'list.html', {'list': list_})
+		try:
+			item = Item(text=request.POST['item_text'], list=list_)
+			item.full_clean()
+			item.save()
+			return redirect(f'/lists/{list_.id}/')
+		except ValidationError:
+			error = escape("You can't have an empty list item")
+
+	return render(request, 'list.html', {'list': list_, 'error': error})
 
 def new_list(request):
 	list_ = List.objects.create()
-	item = Item.objects.create(text=request.POST['item_text'], list=list_)
+	item = Item(text=request.POST['item_text'], list=list_)
+
 	try:
 		item.full_clean()
 		item.save()
 	except ValidationError:
 		list_.delete()
-		error = "You can't have an empty list item"
+		error = escape("You can't have an empty list item")
 		return render(request, 'home.html', {"error": error})
 	return redirect(f'/lists/{list_.id}/')
-
