158330d34759cf551b1c38f2081bbbf88b6d46f9
Jessica Gage <jemgage@gmail.com>
2020-08-28 15:05:57 -0600

    Using forms instead of input tags, need to fix broken FTs.

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -4,6 +4,7 @@ from django.test import TestCase
 from django.http import HttpRequest
 from lists.models import Item, List
 from django.utils.html import escape
+from lists.forms import ItemForm
 
 from lists.views import home_page
 
@@ -11,6 +12,10 @@ class HomePageTest(TestCase):
 	def test_uses_home_template(self):
 		response = self.client.get('/')
 		self.assertTemplateUsed(response, 'home.html')
+
+	def test_home_page_uses_item_form(self):
+		response = self.client.get('/')
+		self.assertIsInstance(response.context['form'], ItemForm)
 
 class ListViewTest(TestCase):
 	def test_passes_correct_list_to_template(self):
--- lists/views.py
+++ lists/views.py
@@ -3,8 +3,10 @@ from django.core.exceptions import ValidationError
 from lists.models import Item, List
 from django.utils.html import escape
 
+from lists.forms import ItemForm
+
 def home_page(request):
-	return render(request, 'home.html')
+	return render(request, 'home.html', {'form': ItemForm()})
 
 def view_list(request, list_id):
 	list_ = List.objects.get(id=list_id)
