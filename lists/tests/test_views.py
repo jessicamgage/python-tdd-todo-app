1f94f5223258d3923e99c35f7c0a0dfcc5f64d84
Jessica Gage <jemgage@gmail.com>
2020-09-01 13:29:49 -0600

    Beginning to lay the foundation for the use of JavaScript for client-side regulation. One failing functional test with an error message.

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -9,15 +9,6 @@ from unittest import skip
 
 from lists.views import home_page
 
-class HomePageTest(TestCase):
-	def test_uses_home_template(self):
-		response = self.client.get('/')
-		self.assertTemplateUsed(response, 'home.html')
-
-	def test_home_page_uses_item_form(self):
-		response = self.client.get('/')
-		self.assertIsInstance(response.context['form'], ItemForm)
-
 class ListViewTest(TestCase):
 	def test_passes_correct_list_to_template(self):
 		other_list = List.objects.create()
