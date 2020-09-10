b0c6b3d035c0b681ef74c02c8de24129eaa7a842
Jessica Gage <jemgage@gmail.com>
2020-09-10 15:39:47 -0600

    Got the mock for a send_mail() function test working.

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+import accounts.views
 
 class SendLoginEmailViewTest(TestCase):
 	def test_redirects_to_home_page(self):
@@ -6,3 +7,24 @@ class SendLoginEmailViewTest(TestCase):
 			'email': 'onlyforbook61@gmail.com'
 		})
 		self.assertRedirects(response, '/')
+
+	def test_sends_mail_to_address_from_post(self):
+		self.send_mail_called = False
+
+		def fake_send_login_email(subject, body, from_email, to_list):
+			self.send_mail_called = True
+			self.subject = subject
+			self.body = body
+			self.from_email = from_email
+			self.to_list = to_list
+
+		accounts.views.send_mail = fake_send_login_email
+
+		self.client.post('/accounts/send_login_email', data={
+			'email': 'onlyforbook61@gmail.com'
+		})
+
+		self.assertTrue(self.send_mail_called)
+		self.assertEqual(self.subject, 'Your login link for TheToDoListSite')
+		self.assertEqual(self.from_email, 'noreply@thetodolistsite')
+		self.assertEqual(self.to_list, ['onlyforbook61@gmail.com'])
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,12 @@
 from django.core.mail import send_mail
 from django.shortcuts import redirect
 
-def send_login_email(self):
+def send_login_email(request):
+	email = request.POST['email']
+	send_mail(
+		'Your login link for TheToDoListSite', 
+		'body text tbc', 
+		'noreply@thetodolistsite', 
+		[email]
+	)
 	return redirect('/')
