a2f5f344d1195be65d9b73f28e7948a9cb1445a9
Jessica Gage <jemgage@gmail.com>
2020-09-17 15:32:13 -0600

    Refactored out code relevant to add_list_item, as well as fixing errors caused by it by attempting to use it to add duplicate list items. Back to one failing FT.

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -51,41 +51,39 @@ class ItemValidationTest(FunctionalTest):
 
 		self.get_item_input_box().send_keys(Keys.ENTER)
 		self.wait_for_row_in_list_table('1: Buy almond milk')
-		self.wait_for_row_in_list_table('2: Make tea')	
+		self.wait_for_row_in_list_table('2: Make tea')
 
 	def test_cannot_add_duplicate_items(self):
-		#Edith goes to the home page and starts a new list
+		#Edith goes to the home page to start a new list
 		self.browser.get(self.live_server_url)
-
 		self.add_list_item('Buy wellies')
-
+		
 		#She accidentally tries to enter a duplicate item
-		self.add_list_item('Buy wellies')
-
+		self.get_item_input_box().send_keys('Buy wellies')
+		self.get_item_input_box().send_keys(Keys.ENTER)
+		
 		#She sees a helpful error message
 		self.wait_for(lambda: self.assertEqual(
-			self.get_error_element().text,
+			self.browser.find_element_by_css_selector('.has-error').text,	
 			"You've already got this in your list"
 		))
 
 	def test_error_messages_are_cleared_on_input(self):
-		#Edith starts a list and causes a validation error
-		
+		#Edith starts a list and causes a validation error:
 		self.browser.get(self.live_server_url)
+		self.add_list_item('Banter too thick')
+
+		self.get_item_input_box().send_keys('Banter too thick')
+		self.get_item_input_box().send_keys(Keys.ENTER)
 		
-		self.add_list_item('Banter too thick')
-		self.add_list_item('Banter too thick')
-	
 		self.wait_for(lambda: self.assertTrue(
-			self.get_error_element().is_displayed()
-		))		
+			self.browser.find_element_by_css_selector('.has-error').is_displayed()
+		))
 
 		#She starts typing in the input box to clear the error
-
 		self.get_item_input_box().send_keys('a')
 
-		#She is pleased to see that the error message disappears
-
+		#She is pleased to see the error message goes away
 		self.wait_for(lambda: self.assertFalse(
-			self.get_error_element().is_displayed()
+			self.browser.find_element_by_css_selector('.has-error').is_displayed()
 		))
