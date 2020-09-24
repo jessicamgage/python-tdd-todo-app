6f11fd0d9fc58d308e5a42f8295a72e3745d2afd
Jessica Gage <jemgage@gmail.com>
2020-09-24 15:40:34 -0600

    Added a FT for a user to share their list with another logged-in user. Running into expected failures.

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -1,15 +1,17 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from django.conf import settings
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
-
 from unittest import skip
-
 from selenium.common.exceptions import WebDriverException
+from django.contrib.auth import BACKEND_SESSION_KEY, SESSION_KEY, get_user_model
+from django.contrib.sessions.backends.db import SessionStore
 
 import os
 
 MAX_WAIT = 10
+User = get_user_model()
 
 class FunctionalTest(StaticLiveServerTestCase):
 	def setUp(self):
@@ -36,7 +38,23 @@ class FunctionalTest(StaticLiveServerTestCase):
 					if time.time() - start_time > MAX_WAIT:
 						raise e
 					time.sleep(0.5)
+	
 		return modified_fn
+	
+	def create_pre_authenticated_session(self, email):
+                user = User.objects.create(email=email)
+                session = SessionStore()
+                session[SESSION_KEY] = user.pk
+                session[BACKEND_SESSION_KEY] = settings.AUTHENTICATION_BACKENDS[0]
+                session.save()
+
+                self.browser.get(self.live_server_url + "/404_no_such_url/")
+                self.browser.add_cookie(dict(
+                        name=settings.SESSION_COOKIE_NAME,
+                        value=session.session_key,
+                        path='/',
+                ))
+
 	
 	@wait
 	def wait_for_row_in_list_table(self, row_text):
