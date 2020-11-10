48cdf59a600125997c2a982bb0aa1cb215716f5e
Jessica Gage <61759770+jessicamgage@users.noreply.github.com>
2020-11-10 15:29:46 -0700

    Update apps.py

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,5 +1,9 @@
-from django.apps import AppConfig
+"""
+The purpose of the apps.py file is to allow for a user to not have to manually configure apps via the INSTALLED_APPS bracket in settings.py.
+In this case, the configuration for accounts is done in this file.
+"""
 
+from django.apps import AppConfig
 
 class AccountsConfig(AppConfig):
     name = 'accounts'
