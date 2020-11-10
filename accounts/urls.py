61a7f17d6642320613f72fb78c0cb9dbb57a9d40
Jessica Gage <61759770+jessicamgage@users.noreply.github.com>
2020-11-10 15:24:54 -0700

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,3 +1,7 @@
+"""
+These URLs are specific to the accounts route -- any URL that is accessed via the accounts route, such as sending a login email, is used with these URLs.
+So, unlike the urls.py file in superlists, these are not site-wide URLs.
+"""
 from django.conf.urls import url
 from django.contrib.auth.views import logout
 from accounts import views
