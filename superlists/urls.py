b59a44ff246fb42da1266b041060d03f31401572
Jessica Gage <61759770+jessicamgage@users.noreply.github.com>
2020-11-10 14:30:23 -0700

    Updated urls.py
    Updated to include descriptive comment of file use.

--- superlists/urls.py
+++ superlists/urls.py
@@ -1,3 +1,5 @@
+//This file wires up site-wide URLs with the usage of regular expressions, URLs for separate routes using list and account URLs, and the views pulled from views.py
+
 from django.conf.urls import include, url
 from lists import views as list_views
 from lists import urls as list_urls
