d4ccb5dd5778f26a65ecd73afcfe6b7c46468e2b
Jessica Gage <61759770+jessicamgage@users.noreply.github.com>
2020-11-10 15:17:43 -0700

    Update admin.py

--- lists/admin.py
+++ lists/admin.py
@@ -1,3 +1,7 @@
+"""
+I chose not to use this file -- it allows for registration of models, and I chose to not have it as a component of my app.
+"""
+
 from django.contrib import admin
 
 # Register your models here.
