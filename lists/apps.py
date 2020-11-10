fb00eb18a3fa53a530c96475c46faa1199553729
Jessica Gage <61759770+jessicamgage@users.noreply.github.com>
2020-11-10 15:00:01 -0700

    Update apps.py

--- lists/apps.py
+++ lists/apps.py
@@ -1,3 +1,7 @@
+"""
+This file stores metadata for Django. While minimalistic, it is a pwoerful tool that allows users to not have to manually update their INSTALLED_APPS via Django to make use of these configurations.
+"""
+
 from django.apps import AppConfig
 
 
