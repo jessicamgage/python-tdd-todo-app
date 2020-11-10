55cb736911eb8c470668bc9580d0b1973bfa56a5
Jessica Gage <61759770+jessicamgage@users.noreply.github.com>
2020-11-10 15:30:44 -0700

    Update admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,8 @@
+"""
+The purpose of an admin.py file is to allow for the official registration of certain apps.
+I have chosen not to officially register any of my models, so this file remains blank for the user to do as they please with it.
+"""
+
 from django.contrib import admin
 
 # Register your models here.
